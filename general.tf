ed172ce41be788b37d419a8e1a0359e3bf1b6263
sim <sim@sim.com>
2024-09-04 21:13:09 +0300

    fixfix

--- general.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "main-rg" {
-  name     = "mainResourceGroup"
-  location = "eastus"
-}
--- provider.tf
+++ provider.tf
@@ -3,10 +3,18 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.51.0"
+      source = "hashicorp/azurerm"      
     }
     
+  }
+
+  backend "azurerm" {
+    #resource_group_name  = "general-rg"
+    #storage_account_name = "simgeneraltfstatestorage"
+    #container_name       = "tf-stat-container"
+    #key                  = "main.tfstate"
+    #tenant_id            = "f916afa5-dd10-4aaf-aa50-e757519da337"
+    #subscription_id      = "b799688b-a05a-46da-a97e-78ebae44a60e"
   }
 
 }
@@ -14,3 +22,6 @@ terraform {
 provider "azurerm" {
     features { }
 }
+
+
+
