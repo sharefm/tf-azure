98b51d445f2cc9778aad561024ada33f8f3eea3d
sim <sim@sim.com>
2024-09-05 01:38:20 +0300

    10.0.0.0

--- aks-main.tf
+++ aks-main.tf
@@ -18,7 +18,7 @@ resource "azurerm_subnet" "main-cluster-subnet" {
   name                 = "main-cluster-subnet"
   resource_group_name  = azurerm_resource_group.main-rg.name
   virtual_network_name = azurerm_virtual_network.main-vnet.name  
-  address_prefixes     = ["172.16.1.0/24"]
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
 
