4a7eeea161005f35836c9746f7125ea3273f0e2f
sim <sim@sim.com>
2024-09-04 19:15:12 +0300

    tftftf

--- .github/workflows/provider.tf
+++ .github/workflows/provider.tf
@@ -1,4 +1,16 @@
 # Configure the Azure Provider
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.51.0"
+    }
+    
+  }
+
+}
+
 provider "azurerm" {
     features { }
 }
