d545ff0b08e8aa2b1b330ac64afd572b495912aa
sim <sim@sim.com>
2024-09-05 00:35:20 +0300

    backend

--- provider.tf
+++ provider.tf
@@ -9,14 +9,14 @@ terraform {
     
   }
 
-  # backend "azurerm" {
-  #   #resource_group_name  = "general-rg"
-  #   #storage_account_name = "simgeneraltfstatestorage"
-  #   #container_name       = "tf-stat-container"
-  #   #key                  = "main.tfstate"
-  #   #tenant_id            = "f916afa5-dd10-4aaf-aa50-e757519da337"
-  #   #subscription_id      = "b799688b-a05a-46da-a97e-78ebae44a60e"
-  # }
+  backend "azurerm" {
+    #resource_group_name  = "general-rg"
+    #storage_account_name = "simgeneraltfstatestorage"
+    #container_name       = "tf-stat-container"
+    #key                  = "main.tfstate"
+    #tenant_id            = "f916afa5-dd10-4aaf-aa50-e757519da337"
+    #subscription_id      = "b799688b-a05a-46da-a97e-78ebae44a60e"
+  }
 
 }
 
