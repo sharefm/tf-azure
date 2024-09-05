28ab9833935ee12fd85fa3263034a945ead8473e
sim <sim@sim.com>
2024-09-05 06:52:31 +0300

    10.244.0.0/16

--- aks-main.tf
+++ aks-main.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "main-vnet" {
   name                = "main-vnet"
   location            = azurerm_resource_group.main-rg.location
   resource_group_name = azurerm_resource_group.main-rg.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.10.0.0/16"]
 }
 
 # Create a Subnet
@@ -18,14 +18,14 @@ resource "azurerm_subnet" "main-cluster-subnet" {
   name                 = "main-cluster-subnet"
   resource_group_name  = azurerm_resource_group.main-rg.name
   virtual_network_name = azurerm_virtual_network.main-vnet.name  
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.10.1.0/24"]
 }
 
 resource "azurerm_subnet" "cluster-subnet" {
   name                 = "cluster-subnet"
   resource_group_name  = azurerm_resource_group.main-rg.name
   virtual_network_name = azurerm_virtual_network.main-vnet.name  
-  address_prefixes     = ["10.0.3.0/24"]
+  address_prefixes     = ["10.10.3.0/24"]
 }
 
 
@@ -45,7 +45,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     auto_scaling_enabled = true
     min_count           = 1
     max_count           = 10
-    #vnet_subnet_id      = azurerm_subnet.cluster-subnet.id
+    vnet_subnet_id      = azurerm_subnet.cluster-subnet.id
   }
 
   identity {
