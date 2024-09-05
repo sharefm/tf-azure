cb55595f41209b013d57eb69745931e186f1323e
sim <sim@sim.com>
2024-09-05 13:58:00 +0300

    10.10

--- aks-main.tf
+++ aks-main.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "main-vnet" {
   name                = "main-vnet"
   location            = azurerm_resource_group.main-rg.location
   resource_group_name = azurerm_resource_group.main-rg.name
-  address_space       = ["172.16.0.0/16"]
+  address_space       = ["10.10.0.0/16"]
 }
 
 # Create a Subnet
